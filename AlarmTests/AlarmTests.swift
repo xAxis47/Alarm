cb4c8726006ee8bca0d649d1f5d70a8e91134c2b
xAxis47 <wataru.kawagoe.dev@gmail.com>
2025-01-25 10:32:54 +0900

    Initial Commit

---
+++ AlarmTests/AlarmTests.swift
@@ -0,0 +1,16 @@
+//
+//  AlarmTests.swift
+//  AlarmTests
+//
+//  Created by Kawagoe Wataru on 2025/01/25.
+//
+
+import Testing
+
+struct AlarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
